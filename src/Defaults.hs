c322fa19c8b3019ef0ced799b54cc321d9c7299b
Kavi Gupta <kavi@globalmbox.com>
2017-01-11 18:50:59 -0800

    Moved most of the instances of FromObject, FromStack, ToStack, etc. to their own class

--- src/Defaults.hs
+++ src/Defaults.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE TypeSynonymInstances, FlexibleInstances, UndecidableInstances, IncoherentInstances #-}
 module Defaults(indexBuiltinFunction) where
 
 import Prelude hiding (lookup)
@@ -8,6 +7,7 @@ import Data.Char
 
 import Environment
 import Parser
+import StackManip
 
 builtins :: Map String (InterpAct ())
 builtins = fromList [
@@ -107,11 +107,6 @@ cons (TwoStack x y) = (x, y)
 deCons :: (Object, Object) -> TwoStack Object Object
 deCons (x, y) = TwoStack x y
 
-dedotify :: Object -> Object -> [Object]
-dedotify car Nil = [car]
-dedotify car (Pair cadr cddr) = car : dedotify cadr cddr
-dedotify car cdr = [car, cdr]
-
 stackCurry :: (a -> b -> c) -> TwoStack a b -> c
 stackCurry f (TwoStack x y) = f x y
 
@@ -119,93 +114,3 @@ numberOperator :: (Integer -> Integer -> Integer) -> InterpAct ()
 numberOperator (#) = stackCurry (#) <|> err
     where err = pop >>= \x -> pop >>= \y -> throwError . BuiltinTypeError $
                 "Builtin requires two Integers but received " ++ show x ++ " and " ++ show y
-
-class ToObject a where
-    toObject :: a -> Object
-
-instance (ToObject a) => ToStack a where
-    toStack = push . toObject
-
-class ToStack a where
-    toStack :: a -> InterpAct ()
-
-data TwoStack a b = TwoStack a b
-
-data NoStack = NoStack
-
-instance (ToStack a, ToStack b) => ToStack (TwoStack a b) where
-    toStack (TwoStack x y) = toStack x >> toStack y
-
-instance ToStack NoStack where
-    toStack NoStack = return ()
-
-instance (ToStack a) => ToStack (IO a) where
-    toStack v = liftIO v >>= toStack
-
-instance ToObject Integer where
-    toObject = Number
-
-instance ToObject Object where
-    toObject = id
-
-instance ToObject String where
-    toObject = Str
-
-instance (ToObject a, ToObject b) => ToObject (a, b) where
-    toObject (a, b) = Pair (toObject a) (toObject b)
-
-instance (ToObject a) => ToObject [a] where
-    toObject = foldr (Pair . toObject) Nil
-
-class FromObject a where
-    fromObject :: Object -> Maybe a
-
-class FromStack a where
-    fromStack :: InterpAct (Maybe a)
-
-instance {-# OVERLAPPABLE #-} (FromObject a) => FromStack a where
-    fromStack = do
-        x <- pop
-        case fromObject x of
-            Just y -> return (Just y)
-            Nothing -> push x >> return Nothing
-
-instance (FromObject Object) where
-    fromObject = Just
-
-instance (FromObject Integer) where
-    fromObject (Number x') = Just x'
-    fromObject _ = Nothing
-
-instance (FromObject Int) where
-    fromObject = fmap fromInteger . fromObject
-
-instance (FromObject String) where
-    fromObject (Str x') = Just x'
-    fromObject _ = Nothing
-
-instance (FromObject a) => FromObject [a] where
-    fromObject Nil = return []
-    fromObject (Pair a b) = mapM fromObject $ dedotify a b
-    fromObject (Str s) = mapM (fromObject . cTn) s
-        where
-        cTn = Number . toInteger . ord
-    fromObject _ = Nothing
-
-instance (FromStack a, FromStack b) => (FromStack (TwoStack a b)) where
-    fromStack = do
-        x <- fromStack
-        y <- fromStack
-        return $ liftM2 TwoStack x y
-
-instance (FromObject a, FromObject b) => (FromObject (a, b)) where
-    fromObject (Pair b a) = liftM2 (,) (fromObject a) (fromObject b)
-    fromObject _ = Nothing
-
-infixr 9 <|>
-(<|>) :: (FromStack a, ToStack b) => (a -> b) -> InterpAct () -> InterpAct ()
-f <|> other = do
-    first <- fromStack
-    case first of
-        Just v -> toStack $ f v
-        Nothing -> other
---
+++ src/StackManip.hs
@@ -0,0 +1,102 @@
+{-# LANGUAGE TypeSynonymInstances, FlexibleInstances, UndecidableInstances, IncoherentInstances #-}
+module StackManip(ToStack(..), TwoStack(..), NoStack(..), (<|>)) where
+
+import Control.Monad.Except
+import Data.Char
+
+import Environment
+
+class ToObject a where
+    toObject :: a -> Object
+
+class ToStack a where
+    toStack :: a -> InterpAct ()
+
+data TwoStack a b = TwoStack a b
+
+data NoStack = NoStack
+
+class FromObject a where
+    fromObject :: Object -> Maybe a
+
+class FromStack a where
+    fromStack :: InterpAct (Maybe a)
+
+infixr 9 <|>
+(<|>) :: (FromStack a, ToStack b) => (a -> b) -> InterpAct () -> InterpAct ()
+f <|> other = do
+    first <- fromStack
+    case first of
+        Just v -> toStack $ f v
+        Nothing -> other
+
+instance (ToObject a) => ToStack a where
+    toStack = push . toObject
+
+instance (ToStack a, ToStack b) => ToStack (TwoStack a b) where
+    toStack (TwoStack x y) = toStack x >> toStack y
+
+instance ToStack NoStack where
+    toStack NoStack = return ()
+
+instance (ToStack a) => ToStack (IO a) where
+    toStack v = liftIO v >>= toStack
+
+instance ToObject Integer where
+    toObject = Number
+
+instance ToObject Object where
+    toObject = id
+
+instance ToObject String where
+    toObject = Str
+
+instance (ToObject a, ToObject b) => ToObject (a, b) where
+    toObject (a, b) = Pair (toObject a) (toObject b)
+
+instance (ToObject a) => ToObject [a] where
+    toObject = foldr (Pair . toObject) Nil
+
+instance {-# OVERLAPPABLE #-} (FromObject a) => FromStack a where
+    fromStack = do
+        x <- pop
+        case fromObject x of
+            Just y -> return (Just y)
+            Nothing -> push x >> return Nothing
+
+instance (FromObject Object) where
+    fromObject = Just
+
+instance (FromObject Integer) where
+    fromObject (Number x') = Just x'
+    fromObject _ = Nothing
+
+instance (FromObject Int) where
+    fromObject = fmap fromInteger . fromObject
+
+instance (FromObject String) where
+    fromObject (Str x') = Just x'
+    fromObject _ = Nothing
+
+instance (FromObject a) => FromObject [a] where
+    fromObject Nil = return []
+    fromObject (Pair a b) = mapM fromObject $ dedotify a b
+    fromObject (Str s) = mapM (fromObject . cTn) s
+        where
+        cTn = Number . toInteger . ord
+    fromObject _ = Nothing
+
+instance (FromStack a, FromStack b) => (FromStack (TwoStack a b)) where
+    fromStack = do
+        x <- fromStack
+        y <- fromStack
+        return $ liftM2 TwoStack x y
+
+instance (FromObject a, FromObject b) => (FromObject (a, b)) where
+    fromObject (Pair b a) = liftM2 (,) (fromObject a) (fromObject b)
+    fromObject _ = Nothing
+
+dedotify :: Object -> Object -> [Object]
+dedotify car Nil = [car]
+dedotify car (Pair cadr cddr) = car : dedotify cadr cddr
+dedotify car cdr = [car, cdr]
